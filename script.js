def146e1e58eae994592b8e5c2e9aa6441682d27
Chutong Deng <alanadengchutong@outlook.com>
2025-07-08 02:01:49 -0400

    update

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ function showContact() {
     contact.style.display = contact.style.display === "none" ? "block" : "none";
 }
 
+
+function toggleMenu() {
+  const menu = document.getElementById('navMenu');
+  menu.classList.toggle('show');
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const dropdownToggle = document.querySelector(".dropdown > a");
-  const dropdownMenu = document.querySelector(".dropdown-menu");
+  const dropdownMenu = document.getElementById("dropdownMenu");
 
   dropdownToggle.addEventListener("click", function (e) {
     e.preventDefault();
@@ -13,7 +19,10 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   document.addEventListener("click", function (e) {
-    if (!dropdownToggle.contains(e.target) && !dropdownMenu.contains(e.target)) {
+    if (
+      !dropdownToggle.contains(e.target) &&
+      !dropdownMenu.contains(e.target)
+    ) {
       dropdownMenu.classList.remove("open");
     }
   });
