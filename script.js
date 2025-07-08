eba46183c808f05b7e44d23342069218a0858b6f
Chutong Deng <alanadengchutong@outlook.com>
2025-07-08 02:14:36 -0400

    update

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function showContact() {
     const contact = document.getElementById("contact-info");
     contact.style.display = contact.style.display === "none" ? "block" : "none";
 }
+
+function toggleDropdown() {
+  const dropdown = document.getElementById("dropdownMenu");
+  dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+}
+
+
+document.addEventListener('click', function(event) {
+    const dropdown = document.getElementById("dropdownMenu");
+    const moreBtn = event.target.closest('a');
+
+    if (!event.target.closest('.dropdown') && dropdown.style.display === 'block') {
+        dropdown.style.display = 'none';
+    }
+});
